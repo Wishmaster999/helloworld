b82ad297bb1f84f41a4b15d356fda45a4f0f11bc
Andriy <wishmastour@gmail.com>
2022-11-17 19:50:05 +0200

    merge

--- tuples_intro.py
+++ tuples_intro.py
@@ -1,8 +1,8 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+albums = "Welcome to my Nightmare", "Alice Cooper", 1975,
+bad = "Bad Company", "Bad Company", 1974,
+budgie = "Nightflight", "Budgie", 1981,
+imelda = "More Mayhem", "Emilda May", 2011,
+metallica = "Ride the Lightning", "Metallica", 1984,
 
 #print(metallica)
 #print(type(metallica))
